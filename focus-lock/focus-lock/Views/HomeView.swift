bc31319ad84d64698eabb49d2a7c410f534f9134
Suraj Modur <smodur@umd.edu>
2026-01-11 15:21:17 -0500

    Updated HomeView

---
+++ focus-lock/focus-lock/Views/HomeView.swift
@@ -0,0 +1,72 @@
+//
+//  HomeView.swift
+//  
+//
+//  Created by Suraj Modur on 1/11/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            
+            Text("Focus Lock")
+                .font(.largeTitle)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.bottom, 8)
+                    
+            Text(Date(), style: .date)
+                .font(.headline)
+                .foregroundColor(.gray)
+                    
+            Divider()
+                    
+            Text("Today")
+                .font(.title2)
+                .bold()
+                    
+            Text("Money Lost: $15")
+                .foregroundColor(.red)
+            
+            Text("Minutes Wasted: 15 minutes")
+                .foregroundColor(.orange)
+                    
+            Divider()
+                    
+                    
+            Text("This Week")
+                .font(.title2)
+                .bold()
+                    
+            Text("Money Lost: $100")
+                .foregroundColor(.red)
+                    
+            Text("Minutes Wasted: 35 minutes")
+                .foregroundColor(.orange)
+            
+            Divider()
+            
+            Text("Lifetime Stats")
+                .font(.title2)
+                .bold()
+                    
+            Text("Money Lost: $500")
+                .foregroundColor(.red)
+                    
+            Text("Minutes Wasted: 5 hours")
+                .foregroundColor(.orange)
+            
+            
+            
+            Spacer()
+            }
+            .padding()
+        }
+    }
+
+#Preview {
+    HomeView()
+}
---
+++ focus-lock/focus-lock/Views/RulesView.swift
@@ -0,0 +1,18 @@
+//
+//  RulesView.swift
+//  
+//
+//  Created by Suraj Modur on 1/11/26.
+//
+
+import SwiftUI
+
+struct RulesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RulesView()
+}
