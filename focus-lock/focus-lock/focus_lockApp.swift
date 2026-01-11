727849b47fe37ac3ae0a7c10feb91670fade9bde
Shabarish Nair <shabarishnair2020@gmail.com>
2026-01-11 14:01:42 -0500

    swift stuff

---
+++ focus-lock/focus-lock/focus_lockApp.swift
@@ -0,0 +1,17 @@
+//
+//  focus_lockApp.swift
+//  focus-lock
+//
+//  Created by Shabarish Nair on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct focus_lockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
